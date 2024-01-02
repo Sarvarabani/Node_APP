b735b162009c27e4fffe5bbe6ccb053ac1fac171
Sarvarabani <122893981+Sarvarabani@users.noreply.github.com>
2024-01-02 23:46:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello APPS DEPLOYER This is Gouserabbani Shaik!\n'
+  const msg = 'Hello APPS DEPLOYER This is Sarvar Mohammed!\n'
   res.end(msg);
 });
 
